2cd0605884daebd6f76a67e3fea05de34813a629
Bernard Chisumo <bchisumo74@yahoo.com>
2017-02-02 14:02:18 +0000

    Tags and pivot table has been implemented

--- app/Post.php
+++ app/Post.php
@@ -54,4 +54,11 @@ class Post extends Model
             ->get()
             ->toArray();
     }
+
+
+    public  function  tags()
+    {
+        //1 post may have many tags OR Any post may have many tags  //Any tag may be applied to many posts
+        return $this->belongsToMany(Tag::class);
+    }
 }
